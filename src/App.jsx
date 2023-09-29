149fe38ee155e51131390d7ff884293770dac4b4
Micah joshua <micahshegbo1@gmail.com>
2023-09-28 22:31:42 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter } from 'react-router-dom';
+import {} from './components';
+
+const App = () => {
+	return (
+		<BrowserRouter>
+			<div className="relative z-0 bg-primary mx-auto"></div>
+		</BrowserRouter>
+	);
+};
+export default App;
